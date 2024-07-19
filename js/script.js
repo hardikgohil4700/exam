274565823dba815230762f432a0745280a6fe926
hardikgohil4700 <hardikgohil4700>
2024-07-19 11:55:37 +0530

    js-exam

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let fname = document.getElementById('fname');
+let lname = document.getElementById('lname');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let tbody = document.getElementById('View')
